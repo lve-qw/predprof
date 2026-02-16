8fb5de92a401e4d41cd42ec4404c165c5f60b657
lve-qw <shiianovlev@gmail.com>
2026-02-16 18:41:47 +0300

    Update resiver.ino

--- resiver.ino
+++ resiver.ino
@@ -1,25 +1,15 @@
-#include <Servo.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <RF24.h>
 #include <stdio.h>
+#include <RF24.h>
+#include <EEPROM.h>
 
-#define laser_beam A3
-#define btn A0
-#define CE 5
-#define CSN 6
-#define servox 2
-#define servoy 3
-
-Servo myservo;
-[cite_start]Servo myservo1; [cite: 2]
-
-RF24 radio(CE, CSN);
+RF24 radio(9, 10);
 const byte address[6] = "00152";
 
-static int fputchar(const char ch, FILE *stream) {
+const int fputchar(const char ch, FILE *stream) {
   Serial.write(ch);
-  [cite_start]return ch; [cite: 3]
+  return ch;
 }
 static FILE *sstream = fdevopen(fputchar, NULL);
 
@@ -28,63 +18,114 @@ typedef struct {
   int ypos;
   int uid;
   int state;
-[cite_start]} payload; [cite: 4]
+} payload;
 payload res;
 
 void setup() {
   stdout = sstream;
   Serial.begin(115200);
 
-  myservo.attach(servox);
-  myservo1.attach(servoy);
-  mv(0, 0);
-  pinMode(laser_beam, OUTPUT);
-  pinMode(btn, INPUT_PULLUP);
-  [cite_start]radio.begin(); [cite: 5]
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_LOW);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.openReadingPipe(1, address);
+  radio.setPALevel(RF24_PA_MAX);
   radio.startListening();
-}
-int text = 0;
-unsigned long restime = 0;
-unsigned long printtime = 0;
 
-[cite_start]void loop() { [cite: 6]
+  pinMode(8, INPUT_PULLUP);
+
+  loadCalibrationPoints();
+
+  Serial.println("Приемник готов. Ожидание данных...");
+}
+
+bool ok = false;
+int sendAttempt = 0;
+
+int mode = 0;
+const int stepDelay = 1000;
+unsigned long prevMillis = 0;
+int prevMode = 0;
+
+int posesx[36] = {};
+int posesy[36] = {};
+
+void loop() {
   if (radio.available()) {
     radio.read(&res, sizeof(res));
-    [cite_start]printf("xpos %d, ypos %d, state %d\n", res.xpos, res.ypos, res.state); [cite: 7]
-    restime = millis();
-  [cite_start]} [cite: 8]
-  if (millis() - restime >= 1000) {
-    if (millis() - printtime >= 1000) {
-      printf("no data in last %dms\n", (millis() - restime));
-      [cite_start]printtime = millis(); [cite: 9]
-      res.xpos = 0;
-      res.ypos = 0;
-      res.state = 0;
-    }
+
+    printf("\n--- ПРИНЯТО ---\n");
+    printf("UID: K7M9P2XQ\n");
+    printf("xpos: %4d  ypos: %4d\n", res.xpos, res.ypos);
+    printf("State LED: %d\n", res.state);
+
+    res.state = 1; 
+    
+    radio.stopListening();
+    ok = radio.write(&res, sizeof(res));
+    
+    radio.startListening();
+    
+    prevMillis = millis(); 
+    return; 
   }
-  mv(res.xpos, res.ypos);
-  led(res.state);
-[cite_start]} [cite: 10]
 
-void led(int ledstate) {
-  if (ledstate == 0)digitalWrite(laser_beam, 0);
-  else if (ledstate == 1)digitalWrite(laser_beam, 1);
-  else analogWrite(laser_beam, ledstate);
-[cite_start]} [cite: 11]
+  if (mode == 0) {
+    led(1);
+    while (digitalRead(8)) {
+      delay(10);
+    }
+    delay(150);
+    prevMillis = millis();
+    mode = 1;
+    return;
+  }
 
-int curxpos = -1;
-int curypos = -1;
-void mv(int localxpos, int localypos) {
-  int writexpos = map(constrain(localxpos, -90, 90), -90, 90, 0, 180);
-  [cite_start]int writeypos = map(constrain(localypos, -90, 90), -90, 90, 0, 180); [cite: 12]
-  if (writexpos == curxpos && localypos == curypos)return;
-  [cite_start]if (curxpos != writexpos) myservo.write(writexpos); [cite: 13]
-  if (curypos != writeypos) myservo1.write(writeypos);
-  delay(max(abs(writexpos - curxpos), abs(writeypos - curypos)));
-  curxpos = localxpos;
-  [cite_start]curypos = localypos; [cite: 14]
+  if (millis() - prevMillis >= stepDelay) {
+    mode++;
+    prevMillis = millis();
+  }
+
+  if (mode >= 1 && mode <= 36) {
+    mv(posesx[mode - 1], posesy[mode - 1]);
+  } else if (mode > 36) {
+    mv(0, 0);
+    mode = 0;
+    Serial.println("возврат в начальное положение");
+    return;
+  }
+
+  if (prevMode != mode) {
+    prevMode = mode;
+  }
 }
 
+void sendto() {
+  ok = true;
+  sendAttempt = 0;
+}
 
+void loadCalibrationPoints() {
+  const int START_ADDR_X = 0;   
+  const int START_ADDR_Y = 72;  
+
+  for (int i = 0; i < 36; i++) {
+    int tempX; 
+    int tempY;  
+
+    EEPROM.get(START_ADDR_X + i * 2, tempX);  
+    EEPROM.get(START_ADDR_Y + i * 2, tempY);  
+
+    posesx[i] = tempX;
+    posesy[i] = tempY;
+  }
+
+  Serial.println("Калибровочные точки загружены из EEPROM.");
+}
+
+void mv(int x, int y) {
+  res.xpos = x;
+  res.ypos = y;
+}
+void led(int value) {
+  res.state = value;
+}
